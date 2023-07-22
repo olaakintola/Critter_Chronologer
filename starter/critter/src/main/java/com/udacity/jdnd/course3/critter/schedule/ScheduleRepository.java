a1e9fa4beb5db239d3e1b9366f1ab7fffeb7f1f9
Ola Akintola <olanipekun_akin@yahoo.co.uk>
2023-07-22 18:18:37 +0100

    added feature to retrieve schedule for a single pet

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/schedule/ScheduleRepository.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/schedule/ScheduleRepository.java
@@ -8,5 +8,8 @@ import java.util.List;
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     List<Schedule> findByEmployees_Id(long employeeId);
+
+    List<Schedule> findByPets_Id(long petId);
+
     
 }
